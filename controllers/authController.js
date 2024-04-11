88601816fb34e2ca8a2e72bc218487f557877821
malik-saran-haider <137856390+malik-sarran-haider@users.noreply.github.com>
2024-04-11 15:27:21 +0500

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,34 +1,33 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
 const signUp = async (req, res) => {
-  const { username, email, password, profession } = req.body;
+  const { username, email, password, profession } = req.body; // Corrected typo here
 
   try {
-    // Check if the username or email already exists
-    let existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    let user = await User.findOne({ email, username});
 
-    if (existingUser) {
-      return res.status(400).json({ error: 'Username or email already exists' });
+    if (user) {
+      return res.status(400).json({ error: 'User already exists' });
     }
+  
 
-    const newUser = new User({
+    user = new User({
       username,
       email,
       password,
-      profession
+      profession // Corrected typo here
     });
 
     const salt = await bcrypt.genSalt(10);
-    newUser.password = await bcrypt.hash(password, salt);
+    user.password = await bcrypt.hash(password, salt);
 
-    await newUser.save();
+    await user.save();
 
-    res.status(201).json({ success: true });
+    res.status(201).json({ success: 'true' });
   } catch (error) {
     console.error('Error signing up user:', error);
-    res.status(500).json({ error: error.message }); // Send the error message back to the client
+    res.status(500).json({ error: 'Server Error' });
   }
 };
 
@@ -50,24 +49,10 @@ const login = async (req, res) => {
       return res.status(400).json({ error: 'Invalid credentials' });
     }
 
-    // User authenticated successfully, generate JWT token
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    // Return user data along with token
-    res.status(200).json({
-      success: true,
-      token,
-      user: {
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-        profession: user.profession
-        // Add any other fields you want to include
-      },
-    });
+    res.status(200).json({ success: 'true' });
   } catch (error) {
     console.error('Error logging in user:', error);
-    res.status(500).json({ error: error.message }); // Send the error message back to the client
+    res.status(500).json({ error: 'Server Error' });
   }
 };
 
