319780030b1bb288106097a64f97c4abea305777
malik-saran-haider <137856390+malik-sarran-haider@users.noreply.github.com>
2024-04-12 15:38:52 +0500

    updated code

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -3,18 +3,17 @@ const mongoose = require("mongoose");
 
 const postController = {
   Post: async (req, res) => {
-    const {  description, images = [] } = req.body; // Default images to empty array
+    const {  description, images } = req.body; // Updated to only include userId, description, and images
 
     try {
       const post = new Post({
-        userId,
         description,
         images,
       });
 
       await post.save();
 
-      res.status(201).json({ suceess: "true" });
+      res.status(201).json({success: 'true'});
     } catch (error) {
       console.error("Error submitting post:", error);
       res.status(500).json({ error: "Server Error" });
